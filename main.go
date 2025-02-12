1cbb4d27c6d2893a987f2ea2056566cf6c54f8e1
Justin Neubert <justin.neubert@sva.de>
2025-02-12 15:38:35 +0100

    Remove unused import and debug print statement from main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -66,7 +65,6 @@ func Details() models.Plugin {
 }
 
 func handle(req protocol.Request) protocol.Response {
-	fmt.Println(req)
 	// Plugin-specific logic here
 	return protocol.Response{
 		Success: true,
